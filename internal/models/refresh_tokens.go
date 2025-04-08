68364153e4bac398cf9a82190269047d0c6c638e
recrash <1stpkloser@naver.com>
2025-04-08 08:04:13 +0000

    오타 수정

--- internal/models/refresh_tokens.go
+++ internal/models/refresh_tokens.go
@@ -5,6 +5,6 @@ import "time"
 type RefreshToken struct {
 	ID        int       `json:"id"`
 	Email     string    `json:"email"`
-	Token     string    `json:"Token'`
+	Token     string    `json:"token'`
 	ExpiresAt time.Time `json:"expires_at"`
 }
